996b4f6e8b7afd70f3752738888211fb3d3d14cf
Ebencode-x <er.masanja01@gmail.com>
2026-04-18 10:10:39 +0300

    Architect: Added Product CRUD logic

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+# This is our temporary database (The "Heap")
+all_products = []
+
 @app.route('/')
-def home():
-    return render_template('vendor_dashboard.html')
+def dashboard():
+    return render_template('vendor_dashboard.html', products=all_products)
+
+@app.route('/add', methods=['POST'])
+def add_product():
+    # Capture data from the form
+    name = request.form.get('p_name')
+    price = request.form.get('p_price')
+    
+    if name and price:
+        all_products.append({'name': name, 'price': price})
+    
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
